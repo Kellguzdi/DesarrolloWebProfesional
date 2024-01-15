cc895fdf20f0efb88c96b34734247509646a7cb2
kellguzdi <20213tn114@utez.edu.mx>
2024-01-15 15:43:37 -0600

    feat: homework ready

---
+++ src/components/Inicio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>    
+    <b-link :to="{name:'main'}">Siguiente</b-link>
+    <b-breadcrumb router-component-name :items="items"></b-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        items: [
+          {
+            text: 'Inicio',
+            href: '#',
+            active:true
+          },
+          
+        ]
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
